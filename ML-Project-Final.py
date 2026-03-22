b419fc8a75c273c1fef29b8bc4c3fde78a3315be
rifatfatima2209-cmyk <rifatfatima2209@gmail.com>
2026-03-22 12:58:15 +0530

    Add files via upload
    initial upload

---
+++ ML-Project-Final.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+
+
+# In[5]:
+
+
+# 1. Download Data
+ticker = 'infy' # Change to any stock symbol
+data = yf.download(ticker, start='2024-01-01', end='2025-01-01')
+
+
+# In[6]:
+
+
+# 2. Preprocess Data
+# We use 'Close' price for prediction
+df = data[['Close']]
+dataset = df.values
+training_data_len = int(np.ceil(len(dataset) * .8))
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(dataset)
+
+# Create training sequences (using last 60 days to predict the next day)
+train_data = scaled_data[0:int(training_data_len), :]
+x_train, y_train = [], []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+
+# In[7]:
+
+
+# 3. Build the LSTM Model
+model = Sequential([
+    LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)),
+    Dropout(0.2),
+    LSTM(50, return_sequences=False),
+    Dropout(0.2),
+    Dense(25),
+    Dense(1)
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, batch_size=32, epochs=5)
+
+
+# In[8]:
+
+
+# 4. Testing & Prediction
+test_data = scaled_data[training_data_len - 60: , :]
+x_test, y_test = [], dataset[training_data_len:, :]
+
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions) # Undo scaling
+
+
+# In[9]:
+
+
+# 5. Visualize
+train = df[:training_data_len]
+valid = df[training_data_len:]
+valid['Predictions'] = predictions
+
+plt.figure(figsize=(12,6))
+plt.title(f'{ticker} Stock Price Prediction')
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Actual', 'Predicted'], loc='lower right')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
